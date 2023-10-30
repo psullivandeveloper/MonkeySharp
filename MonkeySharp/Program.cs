6ada77c2452ea2eca2b0b7166a4c1e554e9b9335
Patrick Sullivan <psullivandeveloper@gmail.com>
2023-10-30 13:27:41 -0600

    Finished chapter 1. Have a repl now that prints out the token types and their literals.

--- MonkeySharp/Program.cs
+++ MonkeySharp/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using MonkeySharp;
+
 Console.WriteLine("Hello, World!");
+
+Repl.Start();
+
---
+++ MonkeySharp/Repl.cs
@@ -0,0 +1,33 @@
+﻿using MonkeyCore;
+
+namespace MonkeySharp;
+
+public class Repl
+{
+    private const string prompt = ">> ";
+    public static void Start()
+    {
+        bool keepRunning = true;
+
+        while (keepRunning)
+        {
+            Console.Write(prompt);
+            var input = Console.ReadLine();
+            if (input.Trim() == string.Empty)
+            {
+                keepRunning = false;
+            }
+            else
+            {
+                var lexer = new Lexer(input);
+                var token = lexer.NextToken();
+                while (token.TokenType != TokenType.EOF)
+                {
+                    Console.WriteLine($"{token.TokenType} :: {token.Literal}");
+                    token = lexer.NextToken();
+                }
+            }
+        }
+
+    }
+}
